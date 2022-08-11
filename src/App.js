8290ca03a2229be74197b690a4890be51fa8840c
developer <developer@MacBook-Pro-developer.local>
2022-08-11 11:49:58 +0300

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Menu from "./components/Menu";
-import Main from "./components/Main";
-import CreateTodo from "./components/CreateTodo";
-import EditTodo from "./components/EditTodo";
-import Statics from "./components/Statics";
-import s from "./App.css";
+import Main from "./components/Main/Main";
+import CreateTodo from "./components/Main/CreateTodo";
+import EditTodo from "./components/Main/EditTodo";
+import Statics from "./components/Main/Statics";
+import "./App.css";
 function App() {
   return (
     <BrowserRouter>
-      <div className={s.app}>
-        <Menu />
-        <div
-          className={s.content}
-          style={{
-            backgroundColor: "rgba(116, 10, 114, 0.609)",
-          }}
-        >
+      <div className="app">
+        <div className="sidebar">
+          <Menu />
+        </div>
+        <div className="content">
           <Routes>
             <Route path="/" exact={true} element={<Main />} />
             <Route path="/create" element={<CreateTodo />} />

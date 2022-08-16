764768034682228535bb6f55a67031d447e68a25
developer <developer@MacBook-Pro-developer.local>
2022-08-16 16:47:14 +0300

    added handles on Done and Delete btn

--- src/components/todoItemComponent.jsx
+++ src/components/todoItemComponent.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import styles from "./Styleall.module.css";
+
 const TodoItemComponent = ({ todoItem }) => {
-  //   console.log(title);
-  console.log(todoItem);
+  const handleDone = () => {
+    console.log("handleDone", todoItem.id);
+  };
+  const handleDelete = () => {
+    console.log("handleDelete", todoItem.id);
+  };
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.title}>{todoItem.title}</div>
       <div className={styles.buttonsWrapper}>
-        <div className={styles.doneBtn}>Done</div>
-        <div className={styles.deleteBtn}>Delete</div>
+        <div className={styles.doneBtn} onClick={handleDone}>
+          Done
+        </div>
+        <div className={styles.deleteBtn} onClick={handleDelete}>
+          Delete
+        </div>
         <div className={styles.infoBtn}>
           Info
           <div className={styles.description}>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { todoBaseData } from "../mockData";
 import styles from "./pagesstyles.module.css";
 import TodoItemComponent from "../components/todoItemComponent";
+
 const HomePage = () => {
   return (
     <div className={styles.list}>
-      {todoBaseData.map((item, idx) => (
+      {todoBaseData.map((item) => (
         <TodoItemComponent key={item.id} todoItem={item} />
       ))}
     </div>

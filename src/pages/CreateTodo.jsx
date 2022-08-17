a097441b7b7fd8e4d10480c165c7db3413c4b038
developer <developer@MacBook-Pro-developer.local>
2022-08-17 12:18:19 +0300

    upgraded of CreateTodo page  and added styles for this page

--- src/pages/CreateTodo.jsx
+++ src/pages/CreateTodo.jsx
@@ -1,62 +1,80 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import { useState } from "react";
 import styles from "./pagesstyles.module.css";
-{
-  /* <div className={s.error}>
+
+/* <div className={s.error}>
             {errors?.field && <p>{errors?.field?.message}</p>}
           </div>  */
-}
+// we can set mode all but it slows down performance
+
 const CreateTodo = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
     reset,
-  } = useForm();
+  } = useForm({
+    mode: "onBlur",
+  });
   const onSubmit = (data) => {
-    console.log(JSON.stringify(data));
+    console.log(data);
     reset();
   };
   return (
     <div className={styles.wrapChbox}>
+      <h1>Type in your task</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label>Type in your task</label>
-          <div>
-            <input
-              {...register("Title", { required: "Error!" })}
-              placeholder="Title"
-              className={styles.inputText}
-            ></input>
-          </div>
-          <div>
-            <input
-              {...register("Description1", { required: "Error!" })}
-              placeholder="Description"
-              className={styles.inputTextDescr}
-            ></input>
-          </div>
-          <div className={styles.checkInProgress}>
-            <label>In progress</label>
-            <input
-              type="checkbox"
-              {...register("In progress", { required: "Error!" })}
-            ></input>
-          </div>
-          <div className={styles.checkDone}>
-            <label>Done</label>
-            <input
-              type="checkbox"
-              {...register("Done", { required: "Error!" })}
-            ></input>
-          </div>
+        <label>
+          <p>Title:</p>
+          <input
+            {...register("title", {
+              required: "Required field!",
+              minLength: {
+                value: 5,
+                message: "Enter the title",
+              },
+            })}
+            className={styles.inputText}
+          ></input>
+        </label>
+        <div className={styles.error}>
+          {errors?.title && <p>{errors?.title?.message}</p>}
         </div>
-        <div>
-          <button className={styles.submBut} type="submit">
-            Add
-          </button>
+        <label>
+          <p> Description:</p>
+          <textarea
+            {...register("description", {
+              required: "Required field!",
+              minLength: {
+                value: 10,
+                message: "Enter the description",
+              },
+            })}
+            className={styles.inputTextDescr}
+          ></textarea>
+        </label>
+        <div className={styles.error}>
+          {errors?.description && <p>{errors?.description?.message}</p>}
         </div>
+        <label>
+          In progress
+          <input
+            value="false"
+            type="radio"
+            {...register("isCompleted", { required: true })}
+          ></input>
+        </label>
+        <label>
+          Done
+          <input
+            value="true"
+            type="radio"
+            {...register("isCompleted", { required: true })}
+          ></input>
+        </label>
+        <button className={styles.submBut} type="submit" disabled={!isValid}>
+          Add
+        </button>
       </form>
     </div>
   );
